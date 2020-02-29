340b1f4ec1fd0f0112241013f7c1babc91d5572e
zviktorya <viktorya-92@mail.ru>
2020-02-29 12:38:10 +0300

    Add function getCountElement

---
+++ src/ex2_js-basics-part2/task-03.js
@@ -0,0 +1,20 @@
+function getCountElement(array) {
+    let sumEven = 0;
+    let sumZero = 0;
+    let sumOdd = 0;
+
+    for (let j = 0; j < array.length; j++) {
+        if (typeof array[j] === 'number') {
+            if (array[j] === 0) {
+                sumZero = sumZero + 1;
+
+            } else if (array[j] % 2 === 0) {
+                sumEven++;
+            } else {
+                sumOdd++;
+            }
+        }
+    };
+    return [sumEven, sumOdd, sumZero];
+};
+module.exports = getCountElement;
