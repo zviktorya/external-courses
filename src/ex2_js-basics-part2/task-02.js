524b2b33ce0ef322c67f0b706251d6534f7f7686
zviktorya <viktorya-92@mail.ru>
2020-02-29 12:28:33 +0300

    Add function outputOfArray

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,7 @@
+function outputOfArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log('Element: ' + array[i] + ', Total number of elements: ' + array.length);
+    }
+    return undefined;
+};
+modul.exports = outputOfArray;
