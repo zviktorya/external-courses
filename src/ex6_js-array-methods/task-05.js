48eb153deb172f038269cc6f047d3447afc6e07d
zviktorya <viktorya-92@mail.ru>
2020-03-24 18:03:57 +0300

    Create a function map

---
+++ src/ex6_js-array-methods/task-05.js
@@ -0,0 +1,8 @@
+function map(array, callback) {
+    let newArray = [];
+    array.forEach(function (item, i) {        
+        newArray.push(callback(item, i, array));
+    });
+    return newArray;
+}
+module.exports = map;
